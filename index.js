552a2479d84bf9f89072fc890cc2d1fd94c77850
rym fr <ryymfradi@gmail.com>
2020-10-12 00:11:10 +0100

    step 2

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 
 
-const io = require('socket.io')(4000);
+const io = require('socket.io')(4001);
 
 const users={}
 
 io.on('connection' , socket  =>{
-    socket.on('new user', name =>{
-           users[socket.id] = name
-           socket.broadcast.emit('user-connected', name )
-        })
+    // socket.on('new user', name =>{
+    //        users[socket.id] = name
+    //        socket.broadcast.emit('user-connected', name )
+    //     })
 
 socket.on('send-chat-message' , message => {
- socket.broadcast.emit('chat-message' , {message: message , name : users[socket.id]})
+ socket.broadcast.emit('send-chat-message' , message)
+ 
 })
-// 
+socket.on('typing', data =>{
+    socket.broadcast.emit('typing',data)
+})
 })
 
--- script.js
+++ script.js
@@ -1,42 +1,40 @@
-const socket = io('http://localhost:4000')
-const messageForm = document.getElementById('send-container');
-const messageInput = document.getElementById('message-input');
-const messageContainer = document.getElementById('message-container')
+const socket = io('http://localhost:4001')
 
+const messageForm = document.getElementById('send-container'),
 
+ nameInput = document.getElementById('name'),
 
+ messageInput = document.getElementById('message'),
 
-// 
+ messageContainer = document.getElementById('output'),
+ feedback = document.getElementById('feedback');
 
-const name = prompt("what'is your name ")
-appendMessage('you joind')
-
-socket.emit('new user', name) 
- 
- socket.on('chat-message', data =>{
-     appendMessage(`${data.name} : ${data.message}`)
- })
-socket.on('user-connected', name =>{
-   appendMessage(`${name} is connected`)
-})
-
- messageForm.addEventListener('submit', e =>
+messageForm.addEventListener('submit', e =>
 {
 e.preventDefault();
+const name = nameInput.value
 const  message = messageInput.value
 
-appendMessage(`you : ${message}`)
-socket.emit('send-chat-message', message);
-messageInput.value='';
+socket.emit('send-chat-message', `${name} : ${message}`);
 
+messageContainer.innerHTML += '<li class="me"> you ' + `: ${message}` + '</li>'
+
+messageInput.value='';
+nameInput.value='';
 
 
 })
 
+messageInput.addEventListener('keypress' , ()=>{
+    socket.emit('typing',nameInput.value)
+    feedback.innerHTML = '<p style="float:right ;margin-right:8px;"><em>' + data +  ' is tayping...' + '</em></p>'
+})
+socket.on('typing', data=>{
+   feedback.innerHTML = '<p style="float:left ;margin-left:8px;"><em>' + data +  ' is tayping...' + '</em></p>'
+})
 
-function appendMessage(message){
-    const messageElement = document.createElement('div')
-    messageElement.innerText = message
-    messageContainer.append(messageElement)
-      
-}
+ socket.on('send-chat-message', data =>{
+    feedback.innerHTML ="";
+    messageContainer.innerHTML += '<li class="him">' + data + '</li>'
+ })
+ 
